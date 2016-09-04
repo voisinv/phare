8eeff2ec442ad5d71d28b456c19b309d6c2b7761
voisinv <vincent.voisin@hotmail.fr>
2016-09-04 13:02:39 +0200

    fix hover bug + brightness

--- client/features/dataviz/datavizSrv.js
+++ client/features/dataviz/datavizSrv.js
@@ -7,19 +7,31 @@ const _ = require('lodash');
 //const c10 = d3.scale.category10();
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 let simulation;
+let hideNodes;
 let nodes;
 let links;
 let circleGroup;
 //const scale = d3.scale
-
+let svg;
+var zoom = d3.zoom()
+  .scaleExtent([1, 100])
+  .on('zoom', function() {
+    hideNodes
+      .attr('transform', 'translate(' + (d3.event.transform.x*2) + ',' + (2*d3.event.transform.y) + ') scale(' + (d3.event.transform.k * 2) + ')');
+    nodes
+      .attr('transform', 'translate(' + (d3.event.transform.x*2) + ',' + (2*d3.event.transform.y) + ') scale(' + (d3.event.transform.k * 2) + ')');
+    links
+      .attr('transform', 'translate(' + (d3.event.transform.x*2) + ',' + (2*d3.event.transform.y) + ') scale(' + (d3.event.transform.k * 2) + ')');
+  });
 function createSVG(data) {
-  const svg = d3.select('.visualize-svg')
+  svg = d3.select('.visualize-svg')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .attr('display', 'block')
     .style('margin', 'auto')
-    .style('border', '1px solid black');
+    .style('border', '1px solid black')
+    //.call(zoom);
 
 
   setTimeout(() => {
@@ -46,6 +58,14 @@ function createSVG(data) {
       .attr("x2", (d) => d.target.x)
       .attr("y2", (d) => d.target.y);
 
+    hideNodes = circleGroup
+      .append('circle')
+      .attr('fill', 'white')
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
+      .attr('r', d => d.weight * 5)
+      ;
+
     nodes = circleGroup
       .append('circle')
       .attr('class', 'circle')
@@ -54,14 +74,15 @@ function createSVG(data) {
       .attr('cx', d => d.x)
       .attr('cy', d => d.y)
       .attr('r', d => d.weight * 5)
-      .on('mouseenter', showLabel)
-      .on('mouseleave', hideLabel)
+      .on('mouseenter', showLabelOnMouseEnter)
+      .on('mouseleave', hideLabelOnMouseLeave)
       // put circle on top of svg
       .each(function() {
         d3.select(this.parentNode).raise();
       })
-      .on('click', nodeClicked);
-    circleGroup
+      .on('click', nodeClicked)
+      ;
+    /*circleGroup
       .append('text')
       .attr('fill', 'black')
       .style('opacity', 0)
@@ -74,9 +95,12 @@ function createSVG(data) {
             .attr('dx', ((d.weight * 10) - elem.getBBox().width) / 2));
         return 0;
       })
-      .text(d => _.capitalize(d.value));
+      //.attr('transform', 'translate(' + d3.event.transform.x + ',' + d3.event.transform.y + ') scale(' + d3.event.transform.k + ')')
+
+      .text(d => _.capitalize(d.value));*/
   });
 }
+
 let CURRENT_NODE_INDEX;
 function nodeClicked(e, index) {
   if (index === CURRENT_NODE_INDEX) {
@@ -101,39 +125,83 @@ function nodeClicked(e, index) {
     .style('opacity', 0.3)
     .filter((d, i) => indexOfNodes.indexOf(i) + 1)
     .style('opacity', 1);
+
+
+  displayName(SHOW_ALL_LABEL);
 }
 
-function showLabel() {
-  d3.select(this.parentNode.lastChild)
-    .style('opacity', 1);
+// MUST be nodes object
+function showLabel(d) {
+  d3.select(this).style('opacity', 1);
+  d3.select(this.parentNode.parentNode)
+    .append('text')
+    .attr('fill', 'black')
+    .attr('x', (d.x - (d.weight * 5)))
+    .attr('y', (d.y - (d.weight * 5)) - 5)
+    .attr('dx', function() {
+      var elem = this;
+      setTimeout(() =>
+        d3.select(elem)
+          .transition()
+          .attr('dx', ((d.weight * 10) - elem.getBBox().width) / 2));
+      return 0;
+    })
+    //.attr('transform', 'translate(' + d3.event.transform.x + ',' + d3.event.transform.y + ') scale(' + d3.event.transform.k + ')')
+
+    .text(_.capitalize(d.value));
 }
 
 function hideLabel() {
   if (SHOW_ALL_LABEL) return;
-  d3.select(this.parentNode.lastChild)
-    .transition().style('opacity', 0);
+  d3.select(this).style('opacity', 0.8);
+  d3.select(this.parentNode.parentNode.lastChild)
+    .transition().remove();
 }
 
 function filter(step) {
-  circleGroup
-    .attr('opacity', d => d.weight >= step ? 1 : 0);
-
   links
-    .attr('opacity', d => d.source.weight >= step && d.target.weight >= step ? 0.6 : 0);
+    .style('opacity', d => d.source.weight >= step && d.target.weight >= step ? 0.6 : 0);
+  circleGroup
+    .style('opacity', d => d.weight >= step ? 1 : 0);
+  displayName(SHOW_ALL_LABEL)
 }
 let SHOW_ALL_LABEL = false;
 function displayName(shouldDisplay) {
-  SHOW_ALL_LABEL = shouldDisplay;
-  if (shouldDisplay) {
-    nodes.each(function() {
-      showLabel.call(this)
-    });
+  svg.selectAll('text').remove();
+  let select;
+  if (shouldDisplay && CURRENT_NODE_INDEX !== -1) {
+    select = nodes.filter(shouldDisplayLabelWhenClicked);
   }
   else {
-    nodes.each(function() {
-      hideLabel.call(this)
-    });
+    if (shouldDisplay) {
+      select = nodes.filter(shouldDisplayLabel);
+    }
   }
+  SHOW_ALL_LABEL = shouldDisplay;
+
+  if (select) {
+    select.each(showLabel);
+  }
+}
+
+function showLabelOnMouseEnter(d) {
+  if (shouldDisplayLabelWhenClicked.call(this) && !SHOW_ALL_LABEL) {
+    showLabel.call(this, d);
+  }
+}
+function hideLabelOnMouseLeave(d) {
+  if (shouldDisplayLabelWhenClicked.call(this) && !SHOW_ALL_LABEL) {
+    hideLabel.call(this, d);
+  }
+}
+
+function shouldDisplayLabelWhenClicked() {
+  const parentOpacity = d3.select(this.parentNode).style('opacity');
+  return parentOpacity != 0 && parentOpacity != 0.3;
+}
+
+function shouldDisplayLabel() {
+  return d3.select(this.parentNode).style('opacity') != 0;
 }
 
 function changeWidth(val) {
